2eed95ce753004baac7d96c41e954f3f4cfb29e0
SDreadnoug2 <miltnr01@gmail.com>
2024-09-17 16:46:41 -0400

    update

--- questionboard/src/components/Main/Main.jsx
+++ questionboard/src/components/Main/Main.jsx
@@ -1,12 +1,30 @@
 import "./Main.css"
+import React, { useContext } from "react";
 import Ask from "../Ask/Ask"
 import Answer from "../Answer/Answer"
 import SignOutImage from "../../../public/assets/SignOut.png";
+import SignInImage from "../../../public/assets/SignIn.png";
+import AboutImage from "../../../public/assets/About.png";
+import AnswerImage from "../../../public/assets/Answer.png";
+import AskImage from "../../../public/assets/Ask.png";
+import SettingsImage from "../../../public/assets/Settings.png";
+import { LoggedInContext } from "../../context/LogInContext";
 
 function Main() {
+
+    const isLoggedIn = React.useContext(LoggedInContext);
+
     return (
         <div className="Main">
-            
+            {isLoggedIn ? (
+              <img className="Main__button"src={SignOutImage}/>
+            ) : (
+              <img className="Main__button"src={SignInImage}/>
+            )}
+            <img className="Main__button"src={SignOutImage}/>
+            <img className="Main__button"src={SignOutImage}/>
+            <img className="Main__button"src={SignOutImage}/>
+            <img className="Main__button"src={SignOutImage}/>
             <img className="Main__button"src={SignOutImage}/>
             <h1>bs</h1>
 
